414475874e168f5733a188f1aa2224eee6d04539
ali sinan <alisinan@live.com>
2022-04-27 12:05:12 +0300

    keyshot koltuk 1

---
+++ configurator/koltuk/keyshot-configurator-imagemapping.js
@@ -0,0 +1,883 @@
+imagemappingdata =
+[
+    {
+        "image": "koltuk-4.15.1.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1777"
+        ],
+        "studio": 550919985
+    },
+    {
+        "image": "koltuk-4.15.2.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1810"
+        ],
+        "studio": 550919985
+    },
+    {
+        "image": "koltuk-4.15.3.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1813"
+        ],
+        "studio": 550919985
+    },
+    {
+        "image": "koltuk-4.15.4.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1817"
+        ],
+        "studio": 550919985
+    },
+    {
+        "image": "koltuk-4.15.5.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1777"
+        ],
+        "studio": 550919985
+    },
+    {
+        "image": "koltuk-4.15.6.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1810"
+        ],
+        "studio": 550919985
+    },
+    {
+        "image": "koltuk-4.15.7.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1813"
+        ],
+        "studio": 550919985
+    },
+    {
+        "image": "koltuk-4.15.8.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1817"
+        ],
+        "studio": 550919985
+    },
+    {
+        "image": "koltuk-4.15.9.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1777"
+        ],
+        "studio": 550919985
+    },
+    {
+        "image": "koltuk-4.15.10.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1810"
+        ],
+        "studio": 550919985
+    },
+    {
+        "image": "koltuk-4.15.11.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1813"
+        ],
+        "studio": 550919985
+    },
+    {
+        "image": "koltuk-4.15.12.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1817"
+        ],
+        "studio": 550919985
+    },
+    {
+        "image": "koltuk-4.15.13.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1777"
+        ],
+        "studio": 550919985
+    },
+    {
+        "image": "koltuk-4.15.14.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1810"
+        ],
+        "studio": 550919985
+    },
+    {
+        "image": "koltuk-4.15.15.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1813"
+        ],
+        "studio": 550919985
+    },
+    {
+        "image": "koltuk-4.15.16.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1817"
+        ],
+        "studio": 550919985
+    },
+    {
+        "image": "koltuk-4.15.17.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1777"
+        ],
+        "studio": 550919983
+    },
+    {
+        "image": "koltuk-4.15.18.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1810"
+        ],
+        "studio": 550919983
+    },
+    {
+        "image": "koltuk-4.15.19.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1813"
+        ],
+        "studio": 550919983
+    },
+    {
+        "image": "koltuk-4.15.20.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1817"
+        ],
+        "studio": 550919983
+    },
+    {
+        "image": "koltuk-4.15.21.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1777"
+        ],
+        "studio": 550919983
+    },
+    {
+        "image": "koltuk-4.15.22.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1810"
+        ],
+        "studio": 550919983
+    },
+    {
+        "image": "koltuk-4.15.23.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1813"
+        ],
+        "studio": 550919983
+    },
+    {
+        "image": "koltuk-4.15.24.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1817"
+        ],
+        "studio": 550919983
+    },
+    {
+        "image": "koltuk-4.15.25.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1777"
+        ],
+        "studio": 550919983
+    },
+    {
+        "image": "koltuk-4.15.26.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1810"
+        ],
+        "studio": 550919983
+    },
+    {
+        "image": "koltuk-4.15.27.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1813"
+        ],
+        "studio": 550919983
+    },
+    {
+        "image": "koltuk-4.15.28.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1817"
+        ],
+        "studio": 550919983
+    },
+    {
+        "image": "koltuk-4.15.29.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1777"
+        ],
+        "studio": 550919983
+    },
+    {
+        "image": "koltuk-4.15.30.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1810"
+        ],
+        "studio": 550919983
+    },
+    {
+        "image": "koltuk-4.15.31.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1813"
+        ],
+        "studio": 550919983
+    },
+    {
+        "image": "koltuk-4.15.32.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1817"
+        ],
+        "studio": 550919983
+    },
+    {
+        "image": "koltuk-4.15.33.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1777"
+        ],
+        "studio": 550919984
+    },
+    {
+        "image": "koltuk-4.15.34.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1810"
+        ],
+        "studio": 550919984
+    },
+    {
+        "image": "koltuk-4.15.35.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1813"
+        ],
+        "studio": 550919984
+    },
+    {
+        "image": "koltuk-4.15.36.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1817"
+        ],
+        "studio": 550919984
+    },
+    {
+        "image": "koltuk-4.15.37.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1777"
+        ],
+        "studio": 550919984
+    },
+    {
+        "image": "koltuk-4.15.38.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1810"
+        ],
+        "studio": 550919984
+    },
+    {
+        "image": "koltuk-4.15.39.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1813"
+        ],
+        "studio": 550919984
+    },
+    {
+        "image": "koltuk-4.15.40.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1817"
+        ],
+        "studio": 550919984
+    },
+    {
+        "image": "koltuk-4.15.41.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1777"
+        ],
+        "studio": 550919984
+    },
+    {
+        "image": "koltuk-4.15.42.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1810"
+        ],
+        "studio": 550919984
+    },
+    {
+        "image": "koltuk-4.15.43.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1813"
+        ],
+        "studio": 550919984
+    },
+    {
+        "image": "koltuk-4.15.44.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1817"
+        ],
+        "studio": 550919984
+    },
+    {
+        "image": "koltuk-4.15.45.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1777"
+        ],
+        "studio": 550919984
+    },
+    {
+        "image": "koltuk-4.15.46.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1810"
+        ],
+        "studio": 550919984
+    },
+    {
+        "image": "koltuk-4.15.47.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1813"
+        ],
+        "studio": 550919984
+    },
+    {
+        "image": "koltuk-4.15.48.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1817"
+        ],
+        "studio": 550919984
+    },
+    {
+        "image": "koltuk-4.15.49.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1777"
+        ],
+        "studio": 550919982
+    },
+    {
+        "image": "koltuk-4.15.50.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1810"
+        ],
+        "studio": 550919982
+    },
+    {
+        "image": "koltuk-4.15.51.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1813"
+        ],
+        "studio": 550919982
+    },
+    {
+        "image": "koltuk-4.15.52.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1817"
+        ],
+        "studio": 550919982
+    },
+    {
+        "image": "koltuk-4.15.53.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1777"
+        ],
+        "studio": 550919982
+    },
+    {
+        "image": "koltuk-4.15.54.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1810"
+        ],
+        "studio": 550919982
+    },
+    {
+        "image": "koltuk-4.15.55.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1813"
+        ],
+        "studio": 550919982
+    },
+    {
+        "image": "koltuk-4.15.56.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1817"
+        ],
+        "studio": 550919982
+    },
+    {
+        "image": "koltuk-4.15.57.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1777"
+        ],
+        "studio": 550919982
+    },
+    {
+        "image": "koltuk-4.15.58.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1810"
+        ],
+        "studio": 550919982
+    },
+    {
+        "image": "koltuk-4.15.59.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1813"
+        ],
+        "studio": 550919982
+    },
+    {
+        "image": "koltuk-4.15.60.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1817"
+        ],
+        "studio": 550919982
+    },
+    {
+        "image": "koltuk-4.15.61.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1777"
+        ],
+        "studio": 550919982
+    },
+    {
+        "image": "koltuk-4.15.62.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1810"
+        ],
+        "studio": 550919982
+    },
+    {
+        "image": "koltuk-4.15.63.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1813"
+        ],
+        "studio": 550919982
+    },
+    {
+        "image": "koltuk-4.15.64.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1817"
+        ],
+        "studio": 550919982
+    },
+    {
+        "image": "koltuk-4.15.65.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1777"
+        ],
+        "studio": 550919981
+    },
+    {
+        "image": "koltuk-4.15.66.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1810"
+        ],
+        "studio": 550919981
+    },
+    {
+        "image": "koltuk-4.15.67.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1813"
+        ],
+        "studio": 550919981
+    },
+    {
+        "image": "koltuk-4.15.68.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+59",
+            "12+1817"
+        ],
+        "studio": 550919981
+    },
+    {
+        "image": "koltuk-4.15.69.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1777"
+        ],
+        "studio": 550919981
+    },
+    {
+        "image": "koltuk-4.15.70.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1810"
+        ],
+        "studio": 550919981
+    },
+    {
+        "image": "koltuk-4.15.71.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1813"
+        ],
+        "studio": 550919981
+    },
+    {
+        "image": "koltuk-4.15.72.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1653",
+            "12+1817"
+        ],
+        "studio": 550919981
+    },
+    {
+        "image": "koltuk-4.15.73.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1777"
+        ],
+        "studio": 550919981
+    },
+    {
+        "image": "koltuk-4.15.74.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1810"
+        ],
+        "studio": 550919981
+    },
+    {
+        "image": "koltuk-4.15.75.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1813"
+        ],
+        "studio": 550919981
+    },
+    {
+        "image": "koltuk-4.15.76.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1682",
+            "12+1817"
+        ],
+        "studio": 550919981
+    },
+    {
+        "image": "koltuk-4.15.77.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1777"
+        ],
+        "studio": 550919981
+    },
+    {
+        "image": "koltuk-4.15.78.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1810"
+        ],
+        "studio": 550919981
+    },
+    {
+        "image": "koltuk-4.15.79.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1813"
+        ],
+        "studio": 550919981
+    },
+    {
+        "image": "koltuk-4.15.80.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "10+1723",
+            "12+1817"
+        ],
+        "studio": 550919981
+    }
+];
---
+++ configurator/koltuk/keyshot-configurator-modeldata.js
@@ -0,0 +1,113 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "alcantara%201.png",
+                    "multimaterials": [
+                        "10+59"
+                    ],
+                    "name": "Alcantara 1"
+                },
+                {
+                    "icon": "alcantara%202.png",
+                    "multimaterials": [
+                        "10+1653"
+                    ],
+                    "name": "Alcantara 2"
+                },
+                {
+                    "icon": "alcantara%203.png",
+                    "multimaterials": [
+                        "10+1682"
+                    ],
+                    "name": "Alcantara 3"
+                },
+                {
+                    "icon": "alcantara%204.png",
+                    "multimaterials": [
+                        "10+1723"
+                    ],
+                    "name": "Alcantara 4"
+                }
+            ],
+            "name": "RealCloth Satin Blue #1"
+        },
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "gold%2024k%20polished%20%231.png",
+                    "multimaterials": [
+                        "12+1777"
+                    ],
+                    "name": "Gold 24k Polished #1"
+                },
+                {
+                    "icon": "silver%20polished%20%231.png",
+                    "multimaterials": [
+                        "12+1810"
+                    ],
+                    "name": "Silver Polished #1"
+                },
+                {
+                    "icon": "platinum%20polished%20%231.png",
+                    "multimaterials": [
+                        "12+1813"
+                    ],
+                    "name": "Platinum Polished #1"
+                },
+                {
+                    "icon": "copper%20polished%20%231.png",
+                    "multimaterials": [
+                        "12+1817"
+                    ],
+                    "name": "Copper Polished #1"
+                }
+            ],
+            "name": "Gold 24k Polished #1"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "default.png",
+            "materialgroups": [
+                "RealCloth Satin Blue #1",
+                "Gold 24k Polished #1"
+            ],
+            "modelset": 1004,
+            "name": "Default"
+        }
+    ],
+    "studios": [
+        {
+            "checked": true,
+            "icon": "studio%201.png",
+            "id": 550919981,
+            "name": "Studio 1"
+        },
+        {
+            "icon": "studio%202.png",
+            "id": 550919982,
+            "name": "Studio 2"
+        },
+        {
+            "icon": "studio%203.png",
+            "id": 550919983,
+            "name": "Studio 3"
+        },
+        {
+            "icon": "studio%204.png",
+            "id": 550919984,
+            "name": "Studio 4"
+        },
+        {
+            "icon": "studio%205.png",
+            "id": 550919985,
+            "name": "Studio 5"
+        }
+    ]
+};
